630c4c0d9bd6c333012a63a110a325b5e34fa8ec
Jay Pipaliya <113347625+yudiz-jay@users.noreply.github.com>
2022-10-03 09:30:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,26 +6,42 @@ clubs (♣), diamonds (♦), hearts (♥) and spades (♠)
 let cards = [
     { nLabel: "J", nValue: 11, eSuit: "s", isJoker: false, nGroupId: 0 },
     { nLabel: "Q", nValue: 12, eSuit: "s", isJoker: false, nGroupId: 0 },
-    { nLabel: "K", nValue: 13, eSuit: "s", isJoker: false, nGroupId: 0 },
+    { nLabel: "K", nValue: 13, eSuit: "s", isJoker: true, nGroupId: 0 },
+
     { nLabel: "J", nValue: 11, eSuit: "d", isJoker: false, nGroupId: 1 },
     { nLabel: "Q", nValue: 12, eSuit: "h", isJoker: false, nGroupId: 1 },
     { nLabel: "K", nValue: 13, eSuit: "d", isJoker: false, nGroupId: 1 },
+
     { nLabel: "A", nValue: 1, eSuit: "s", isJoker: false, nGroupId: 2 },
     { nLabel: "2", nValue: 2, eSuit: "s", isJoker: true, nGroupId: 2 },
     { nLabel: "8", nValue: 8, eSuit: "s", isJoker: false, nGroupId: 2 },
     { nLabel: "9", nValue: 9, eSuit: "s", isJoker: false, nGroupId: 2 },
+
     { nLabel: "5", nValue: 5, eSuit: "c", isJoker: false, nGroupId: 3 },
     { nLabel: "6", nValue: 6, eSuit: "c", isJoker: false, nGroupId: 3 },
+
     { nLabel: "2", nValue: 2, eSuit: "d", isJoker: true, nGroupId: 4 },
     { nLabel: "3", nValue: 3, eSuit: "d", isJoker: false, nGroupId: 4 },
     { nLabel: "8", nValue: 8, eSuit: "d", isJoker: false, nGroupId: 4 },
+
     { nLabel: "5", nValue: 5, eSuit: "h", isJoker: false, nGroupId: 5 },
     { nLabel: "6", nValue: 6, eSuit: "h", isJoker: false, nGroupId: 5 },
     { nLabel: "k", nValue: 13, eSuit: "h", isJoker: false, nGroupId: 5 },
+
     { nLabel: "6", nValue: 6, eSuit: "c", isJoker: false, nGroupId: 6 },
     { nLabel: "8", nValue: 8, eSuit: "s", isJoker: false, nGroupId: 6 },
     { nLabel: "7", nValue: 7, eSuit: "d", isJoker: false, nGroupId: 6 },
     { nLabel: "10", nValue: 10, eSuit: "d", isJoker: false, nGroupId: 6 },
+
+    { nLabel: "A", nValue: 1, eSuit: "d", isJoker: false, nGroupId: 7 },
+    { nLabel: "J", nValue: 10, eSuit: "d", isJoker: false, nGroupId:7 },
+    { nLabel: "Q", nValue: 11, eSuit: "d", isJoker: false, nGroupId: 7 },
+    { nLabel: "K", nValue: 12, eSuit: "d", isJoker: false, nGroupId: 7 },
+    { nLabel: "K", nValue: 13, eSuit: "d", isJoker: false, nGroupId: 7 },
+
+    {nLabel: "A",nValue: 1,eSuit: "spades",isJoker: false,nGroupId: 8,},
+    {nLabel: "Q",nValue: 12,eSuit: "spades",isJoker: false,nGroupId: 8,},
+    {nLabel: "K",nValue: 13,eSuit: "spades",isJoker: false,nGroupId: 8,},
 ]
 
 let nCardLength = 0;
@@ -43,21 +59,33 @@ while (nCardLength != cards.length) {
         break;
     }
     document.getElementById("divResult").innerHTML += `<div class="lblScore"></div>`;
+
+    for (let i = 0; i < newCard.length; i++) {
+        if (newCard[i].nValue === 13) {
+            for (let j = 0; j <newCard.length; j++) {
+             if (newCard[j].nValue ===1) {
+                newCard[j].nValue = 14;
+             }     
+            }
+        } 
+    }
+
     
     newCard.sort((a, b) => {
         return a.nValue - b.nValue;
     })
     
-    let strSuit = newCard[0].eSuit;
     let nCardValue = newCard[0].nValue;
+    let strSuit = newCard[0].eSuit;
     let sequence = true;
     for (let i = 0; i < newCard.length; i++) {
-        if ((strSuit !== newCard[i].eSuit) || (nCardValue !== newCard[i].nValue)) {
+        if (((strSuit !== newCard[i].eSuit) || (nCardValue !== newCard[i].nValue))) {
             sequence = false;
             break;
         }
         nCardValue++;
     }
+
 
     if (sequence === false) {
         for (let i = 0; i < newCard.length; i++) {
@@ -79,7 +107,7 @@ while (nCardLength != cards.length) {
     }
     // console.log("--------------------------------");
     // console.log("Score : " + nScore);
-    // console.log(newCard);
+    console.log(newCard);
     
     document.querySelectorAll(".lblScore")[document.querySelectorAll(".lblScore").length-1].innerText = `The Score of Group ${k} - is ${nScore}`;
     
